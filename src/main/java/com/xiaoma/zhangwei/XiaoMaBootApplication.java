929112b69e7561f7f825f0977afa372cc9458d1c
zhangwei2 <zhangwei2@yunzhangfang.com>
2020-12-11 10:44:01 +0800

    first commit

---
+++ src/main/java/com/xiaoma/zhangwei/XiaoMaBootApplication.java
@@ -0,0 +1,16 @@
+package com.xiaoma.zhangwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhangwei
+ * @Description:
+ * @Date:Create：2020/12/11 上午10:30
+ */
+@SpringBootApplication
+public class XiaoMaBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoMaBootApplication.class, args);
+    }
+}
