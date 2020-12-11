483d9213370106bc5d810782c54e6f97a928f99a
zhangwei2 <zhangwei2@yunzhangfang.com>
2020-12-11 10:58:06 +0800

    测试类

---
+++ src/test/java/com/xiaoma/zhangwei/AppTest.java
@@ -0,0 +1,12 @@
+package com.xiaoma.zhangwei;
+
+/**
+ * @Author: zhangwei
+ * @Description:测试类
+ * @Date:Create：2020/12/11 上午10:49
+ */
+public class AppTest {
+    public static void main(String[] args) {
+
+    }
+}
