2b72a8ff053cd01f3f2b429a63c4d54abcd03505
zhangwei2 <zhangwei2@yunzhangfang.com>
2020-12-11 14:42:16 +0800

    mono

--- src/main/java/com/xiaoma/zhangwei/XiaoMaBootApplication.java
+++ src/main/java/com/xiaoma/zhangwei/XiaoMaBootApplication.java
@@ -2,6 +2,20 @@ package com.xiaoma.zhangwei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * @Author: zhangwei
@@ -12,5 +26,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XiaoMaBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(XiaoMaBootApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld(){
+        HandlerFunction<ServerResponse> dateFunction =
+                request -> ok().body(
+                        Mono.just("Today is " + new SimpleDateFormat("yyyy-MM-dd").format(new Date())), String.class);
+        return route(GET("/world"), dateFunction);
     }
 }
